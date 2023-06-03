6a956f10689051534ce53061fcf9302d164b9226
quyetthang22 <thangdqph20775@fpt.edu.vn>
2023-06-03 19:18:06 +0700

    feat: create product refs: AW-2

---
+++ src/app/common/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+  productCode: string;
+  productName: string;
+
+  constructor(productCode: string, productName: string) {
+    this.productCode = productCode;
+    this.productName = productName;
+  }
+}
